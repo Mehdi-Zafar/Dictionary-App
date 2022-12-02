16b05ac63d1287370d1fcf3e2e74330d9e52bf87
Mehdi-Zafar <mehdizaffar0@gmail.com>
2022-12-03 00:08:12 +0500

    3rd commit

--- src/components/Phonetics.jsx
+++ src/components/Phonetics.jsx
@@ -6,7 +6,6 @@ const Phonetics = (props) => {
             {(props.phonetic.length > 0) ? props.phonetic.map((phonetic)=>{
                 if(phonetic.audio !== ''){
                     props.setPhonetic(true)
-                    console.log(phonetic.audio)
                     return(
                         <div className="m-2 shadow-md w-fit rounded-md">
                             {phonetic.audio ?
